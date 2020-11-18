72c91dd0c10e3dcf9fc8e6e6d93892f01d97e185
jack6332 <b10815044@gapps.ntust.edu.tw>
2020-11-18 13:02:03 +0800

    new

--- RollerCoasters-master/src/TrainWindow.cpp
+++ RollerCoasters-master/src/TrainWindow.cpp
@@ -211,7 +211,7 @@ advanceTrain(float dir)
 	// TODO: make this work for your train
 	//#####################################################################
 	if (trainView->tw->arcLength->value()) {
-		m_Track.trainU += 1.0f * speed->value()+ trainView->physical*10;
+		m_Track.trainU += (1.0f * speed->value()+ trainView->physical*10)*dir;
 		if (m_Track.trainU > trainView->Path_Total) {
 			m_Track.trainU -= trainView->Path_Total;
 		}
